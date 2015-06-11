12719c98e136a2810dbe685a17b9ca6ad6b6091b
Gene Kim <genelkim@cs.washington.edu>
2015-06-11 14:46:50 -0700

    Added simple spec test.

--- src/test/scala/edu/knowitall/implie/SpecExampleTest.scala
+++ src/test/scala/edu/knowitall/implie/SpecExampleTest.scala
@@ -6,12 +6,19 @@ package edu.knowitall.implie
  */
 object SpecExampleTest {
   val sentence = "French journalist Paul Legall reported that all three hostages arrived safely at Athens International Airport."
+  val expectedString = "(Athens-13, city, Athens International Airport-15)\n(French-1, nationality, French journalist Paul Legall-4)\n(journalist-2, jobTitle, journalist Paul Legall-4)"
+
   def main(args: Array[String]) {
     val extractor = ImplIELoader.fastImplIE
     val extractions = extractor.extractRelations(sentence)
-    val extractionStrs = extractions.map(e => e.toString)
+    val extractionStrs = extractions.map(e => e.toString).sorted
     println(extractionStrs.mkString("\n"))
-    // TODO: asseert extraction number
-    // TODO: check each extraction
+
+    // There should be three extractions.
+    assert(extractions.size == 3)
+    // Check that the extractions are the expected extractions.
+    assert(extractionStrs.mkString("\n") == expectedString)
+
+    println("PASS: all tests passed!")
   }
 }
